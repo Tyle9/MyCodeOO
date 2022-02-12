7b8798f3f943e7585649b9fdb229732b4a9bcca5
Your Name <you@example.com>
2022-02-12 17:27:31 +0000

    first attempt

---
+++ Assignment3/Chia asssignment/Chia.java
@@ -0,0 +1,21 @@
+public class Chia {
+	private String mood;
+	public Chia(){
+		mood="Happy";
+				}
+	public void talkToChia(){
+		if(mood.equals("Sad")||mood.equals("Terrified"))
+			mood="Happy";
+				}
+	public void yellToChia(){
+		if(mood.equals("Happy")||mood.equals("Terrified"))
+			mood="Sad";
+		}
+	public void singToChia(){
+		if(mood.equals("Happy")||mood.equals("Sad"))
+			mood="Terrified";
+			}
+	public String getMood(){
+		return mood;
+		}
+}
